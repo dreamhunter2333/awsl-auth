0a3ef562a960647012ed66e6e4d37b4207912247
dreamhunter2333 <dreamhunter2333@gmail.com>
2024-04-18 23:35:07 +0800

    feat: add health_check

--- src/health_check_router.py
+++ src/health_check_router.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter, status
 router = APIRouter()
 
 
-@router.get("/health_check", tags=["health check"])
+@router.get("/api/health_check", tags=["health check"])
 def health_check():
     return status.HTTP_200_OK
